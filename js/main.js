287005010a807d298c7ea6caa8fc81b95f4ae1a2
unknown <villalobosyaneth346.com>
2022-12-11 20:00:26 -0600

    remodelaciones en mi body

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const seccionPrincipal = document.getElementById('contenido-1');
 
+const contenedotBotones = document.getElementById('contenedor-btns');
+
 // Inputs de la forma
 const sentimiento = document.getElementById('sentimiento');
 
@@ -18,30 +20,65 @@ form.addEventListener('submit', (e) => {
 
 // Crear un evento global 
 main.addEventListener('click', (e) => {
-  if(e.target.id === 'feliz') {
+
+  if(e.target.classList.contains('feliz-btn')) {
     // Agregar clases a body
     body.removeAttribute('class');
     body.classList.add('feliz');
 
     // Agregar la clase a la seccion
+    seccionPrincipal.classList.remove('enojado-s', 'tranquilo-s', 'triste-s');
     seccionPrincipal.classList.add('feliz-s');
   }
 
-  if(e.target.id === 'triste') {
-    body.removeAttribute('class');
-    body.classList.add('triste');
-  }
+   // llamar a la funcion para agregar el texto
+    insertarEstadoDeAnimo('feliz');
 
-  if(e.target.id === 'tranquilo') {
-    body.removeAttribute('class');
-    body.classList.add('tranquilo');
-  }
 
-  if(e.target.id === 'enojado') {
-    body.removeAttribute('class');
-    body.classList.add('enojado');
+if (e.target.classList.contains('triste-btn')) {
+  body.removeAttribute('class');
+  body.classList.add('triste');
+
+  // Agregar la clase a la seccion
+  seccionPrincipal.classList.remove('enojado-s', 'tranquilo-s', 'feliz-s');
+  seccionPrincipal.classList.add('triste-s');
+
+  // Llamar funcion para agregar texto
+  insertarEstadoDeAnimo('Triste');
+}
+
+if (e.target.classList.contains('tranquilo-btn')) {
+  body.removeAttribute('class');
+  body.classList.add('tranquilo');
+
 
     // Agregar la clase a la seccion
-    seccionPrincipal.classList.add('enojado-s');
+    seccionPrincipal.classList.remove('enojado-s', 'feliz-s', 'triste-s');
+    seccionPrincipal.classList.add('tranquilo-s');
+
+    //llamar para agregar texto
+
+    insertarEstadoDeAnimo('tranquilo');
   }
+
+  if (e.target.classList.contains('enojado-btn')) {
+    body.removeAttribute('class');
+    body.classList.add('enojado');
+ 
+
+    
+    // Agregar la clase a la seccion
+    seccionPrincipal.classList.remove('feliz-s', 'tranquilo-s', 'triste-s');
+    seccionPrincipal.classList.add('enojado-s');
+
+    // Llamar funcion para agregar texto
+    insertarEstadoDeAnimo('Enojado');
+  
+}
+
 });
+
+function insertarEstadoDeAnimo(animo) {
+  const texto = document.getElementById('estado-animo');
+  texto.textContent = animo;
+}
